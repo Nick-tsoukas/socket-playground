612736358a3194b7889086cebcd4a824d2edb7a7
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2020-11-29 14:52:41 -0600

    Sending and receiving data through native websockets

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("This is the js file");
+let ws = new WebSocket('ws://localhost:8000');
+
+ws.onopen = (event) => {
+    ws.send("I am happy that I am now connected to the server")
+}
+
+ws.onmessage = event => {
+   console.log(event);
+}
