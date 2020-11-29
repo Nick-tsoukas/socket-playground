b1e27db204e498b012416c5f2d006d9f8932efa1
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2020-11-29 14:57:18 -0600

    Appending message from socket to html page

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 let ws = new WebSocket('ws://localhost:8000');
-
+let msgElement = document.getElementById('msg');
+console.log(msgElement)
 ws.onopen = (event) => {
     ws.send("I am happy that I am now connected to the server")
 }
 
 ws.onmessage = event => {
-   console.log(event);
+        msgElement.innerText = event.data
+
 }
