a2e5c06361823df0f43de2af56bbe5cd5de237f9
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2020-11-30 17:13:59 -0600

    building layout for simple chat app " ;

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
 // changed port from 8000 to 9000
 const socket = io('http://localhost:9000');
 
-// socket.on('messageFromServer', (dataFromServer) => {
-//     console.log(dataFromServer);
-//     socket.emit('messageToServer', {data: 'this is a message from the CLIENT'} )
-// })
-
 socket.on('connect', () => {
-    console.log("we connected to the web socket");
     socket.on('messageFromServer', (dataFromServer) => {
         console.log(dataFromServer)
     });
