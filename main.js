9dbd5d5139b6c67ac6e9bef90c0d72be0eece223
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2020-11-30 20:36:27 -0600

    Adding group chat room with sockets :

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
+const socket = io('http://localhost:9000');
+
 let form = document.getElementById('message_form');
+let message_list = document.getElementById('message_list');
+socket.on('connect', () => {
+    console.log("we are connected to the socket")
+});
+
+socket.on('messageFromServer', (dataFromServer) => {
+    console.log(dataFromServer)
+});
+
+socket.emit('messageToServer', {data: 'This is a message from the CLIENT'})
+
+
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     const messageElement = document.getElementById('user_message');
     console.log(messageElement.value)
-    socket.emit('message', {data: messageElement.value});
+    socket.emit('message', {text: messageElement.value});
     messageElement.value = '';
-})
-const socket = io('http://localhost:9000');
-
-socket.on('connect', () => {
-    socket.on('messageFromServer', (dataFromServer) => {
-        console.log(dataFromServer)
-    });
-    socket.emit('messageToServer', {data: 'This is a message from the CLIENT'})
 });
+
+socket.on('messageToClients', (msg) => {
+    message_list.innerHTML += `<li>${msg.text.text}</li>`
+})
 
 
